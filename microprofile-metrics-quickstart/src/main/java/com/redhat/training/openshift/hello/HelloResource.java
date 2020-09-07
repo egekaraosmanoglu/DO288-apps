d27be21e721e7e96b342a0a32b95bfc44ade0a8d
rkaraosmanoglu <rkaraosmanoglu.contractor@libertyglobal.com>
2020-09-07 23:03:16 +0200

    for metrics

--- microprofile-metrics-quickstart/src/main/java/com/redhat/training/openshift/hello/HelloResource.java
+++ microprofile-metrics-quickstart/src/main/java/com/redhat/training/openshift/hello/HelloResource.java
@@ -5,18 +5,32 @@ import javax.ws.rs.GET;
 import javax.ws.rs.Path;
 import javax.ws.rs.Produces;
 
+import org.eclipse.microprofile.metrics.*;
+import org.eclipse.microprofile.metrics.annotation.Gauge;
+import org.eclipse.microprofile.metrics.annotation.Metric;
+
 @Path("/")
 public class HelloResource {
+
+
+	@Inject
+	@Metric(name = "requestCount", description = "All JAX-RS request made to the SessionResource",displayName = "SessionResource#requestCount")
+	private Counter requestCount;
+
 
     @GET
     @Path("/hello")
     @Produces("text/plain")
     public String hello() {
+		
+		
+		requestCount.inc();
+		
         String hostname = System.getenv().getOrDefault("HOSTNAME", "unknown");
 	      String message = System.getenv().getOrDefault("APP_MSG", null);
 	      String response = "";
 
-      	if (message == null) {
+      	if (message == null) {	
       	  response = "Hello world from host "+hostname+"\n";
       	} else {
       	  response = "Hello world from host ["+hostname+"].\n";
